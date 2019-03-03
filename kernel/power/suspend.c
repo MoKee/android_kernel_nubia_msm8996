5243080d5e8163dce80d32e89d0c36e2c23703f0
chenzc <dazzworld@163.com>
2019-03-03 21:10:40 +0800

    nx531j: import kernel changes

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -43,6 +43,11 @@ static DECLARE_WAIT_QUEUE_HEAD(suspend_freeze_wait_head);
 enum freeze_state __read_mostly suspend_freeze_state;
 static DEFINE_SPINLOCK(suspend_freeze_lock);
 
+#ifdef CONFIG_ZTEMT_POWER_DEBUG
+bool wakeup_rpm_stats_debug = false;
+bool wakeup_wake_lock_debug = false;
+#endif
+
 void freeze_set_ops(const struct platform_freeze_ops *ops)
 {
 	lock_system_sleep();
@@ -419,6 +424,11 @@ int suspend_devices_and_enter(suspend_state_t state)
 	error = platform_suspend_begin(state);
 	if (error)
 		goto Close;
+
+#ifdef CONFIG_ZTEMT_POWER_DEBUG
+	wakeup_wake_lock_debug = true;
+	wakeup_rpm_stats_debug  = true;
+#endif //CONFIG_ZTEMT_POWER_DEBUG
 
 	suspend_console();
 	suspend_test_start();
