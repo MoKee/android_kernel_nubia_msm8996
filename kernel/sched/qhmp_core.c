497e2dffa2dcd3d185944a0643dac81a595f5a75
chenzc <dazzworld@163.com>
2019-03-22 15:18:32 +0800

    Revert "BACKPORT: sched/topology: Optimize build_group_mask()"
    This reverts commit fe20151ec36d62f9937ac75cd54ac84cd4943d59.

--- kernel/sched/qhmp_core.c
+++ kernel/sched/qhmp_core.c
@@ -7780,12 +7780,12 @@ enum s_alloc {
  */
 static void build_group_mask(struct sched_domain *sd, struct sched_group *sg)
 {
-	const struct cpumask *sg_span = sched_group_cpus(sg);
+	const struct cpumask *span = sched_domain_span(sd);
 	struct sd_data *sdd = sd->private;
 	struct sched_domain *sibling;
 	int i;
 
-	for_each_cpu(i, sg_span) {
+	for_each_cpu(i, span) {
 		sibling = *per_cpu_ptr(sdd->sd, i);
 		if (!cpumask_test_cpu(i, sched_domain_span(sibling)))
 			continue;
