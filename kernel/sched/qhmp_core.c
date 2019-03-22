0806ef3acc58da2b806245686dcda9c7aefadfaa
chenzc <dazzworld@163.com>
2019-03-22 15:15:33 +0800

    Revert "sched/qhmp_core: Make resched_cpu() unconditional"
    This reverts commit b0f6333b7ac95615ff1a84688b3dc422449eb5a5.

--- kernel/sched/qhmp_core.c
+++ kernel/sched/qhmp_core.c
@@ -673,7 +673,8 @@ void resched_cpu(int cpu)
 	struct rq *rq = cpu_rq(cpu);
 	unsigned long flags;
 
-	raw_spin_lock_irqsave(&rq->lock, flags);
+	if (!raw_spin_trylock_irqsave(&rq->lock, flags))
+		return;
 	resched_curr(rq);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
